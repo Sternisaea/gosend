728c52e968b4119aab65a52ee193986824e25ce4
Sternisaea <stern@gnumpf.nl>
2024-10-10 14:52:50 +0200

    Allow option values in settings file to be double quoted

--- src/cmdflags/flags.go
+++ src/cmdflags/flags.go
@@ -149,9 +149,8 @@ func appendOptionsOfFile(opts map[string]string, filePath types.FilePath) (map[s
 			continue
 		}
 		key := strings.ToLower(strings.TrimSpace(line[:equalIndex]))
-		value := strings.TrimSpace(line[equalIndex+1:])
+		value := strings.TrimSpace(strings.Trim(line[equalIndex+1:], "\""))
 		opts[key] = value
-
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("error reading file: %s", err)
