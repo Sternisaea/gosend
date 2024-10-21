f54d02c65009f7a0cb9ee8ebb19386ec9cf8b25a
Sternisaea <stern@gnumpf.nl>
2024-10-21 11:10:24 +0200

    Restructured message code 2nd

--- src/message/content.go
+++ src/message/content.go
@@ -122,33 +122,6 @@ func (msg *Message) getBodyContent() *content {
 	return nil
 }
 
-func (cnt *content) getContentPart(bound string) string {
-	if cnt == nil {
-		return ""
-	}
-	result := ""
-	if bound != "" {
-		result += fmt.Sprintf("--%s\r\n", bound)
-	}
-	for _, h := range (*cnt).headers {
-		result += h + "\r\n"
-	}
-	result += "\r\n"
-	if (*cnt).text != "" {
-		result += (*cnt).text
-		result += "\r\n\r\n"
-	}
-	if (*cnt).parts != nil {
-		for _, p := range *(*cnt).parts {
-			result += (&p).getContentPart((*cnt).boundary)
-		}
-	}
-	if (*cnt).boundary != "" {
-		result += fmt.Sprintf("--%s--\r\n", (*cnt).boundary)
-	}
-	return result
-}
-
 func (msg *Message) getAttachmentContent() ([]content, error) {
 	var cnts []content
 	for i, a := range (*msg).attachments {
@@ -193,6 +166,33 @@ func (msg *Message) getAttachmentContent() ([]content, error) {
 		})
 	}
 	return cnts, nil
+}
+
+func (cnt *content) getContentPart(bound string) string {
+	if cnt == nil {
+		return ""
+	}
+	result := ""
+	if bound != "" {
+		result += fmt.Sprintf("--%s\r\n", bound)
+	}
+	for _, h := range (*cnt).headers {
+		result += h + "\r\n"
+	}
+	result += "\r\n"
+	if (*cnt).text != "" {
+		result += (*cnt).text
+		result += "\r\n\r\n"
+	}
+	if (*cnt).parts != nil {
+		for _, p := range *(*cnt).parts {
+			result += (&p).getContentPart((*cnt).boundary)
+		}
+	}
+	if (*cnt).boundary != "" {
+		result += fmt.Sprintf("--%s--\r\n", (*cnt).boundary)
+	}
+	return result
 }
 
 func getRandomString(length int) string {
